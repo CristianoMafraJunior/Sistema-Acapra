781e2bff52e3274b3656fbb6e7c1d7974e3bbea0
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2025-05-12 19:27:09 -0300

    [IMP] acapra: form cadastrar user

--- acapra/views/cadastrar.py
+++ acapra/views/cadastrar.py
@@ -1,12 +1,31 @@
-from django.shortcuts import render
-
-
+from django.shortcuts import render, redirect
 from acapra.forms import FormCadastrar
+from acapra.models import User
 
 
 def Cadastrar(request):
     if request.method == "POST":
-        form = FormCadastrar(request, data=request.POST)
+        form = FormCadastrar(request.POST)
+
+        nome = request.POST.get("nome")
+        email = request.POST.get("email")
+        senha = request.POST.get("senha")
+        cpf = request.POST.get("cpf")
+        data_nascimento = request.POST.get("data_nascimento")
+        telefone = request.POST.get("telefone")
+
+        user = User(
+            nome=nome,
+            email=email,
+            senha=senha,
+            cpf=cpf,
+            data_nascimento=data_nascimento,
+            telefone=telefone,
+        )
+        user.save()
+
+        return redirect("Home")
+
     else:
         form = FormCadastrar()
 
