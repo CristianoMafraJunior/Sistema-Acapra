0f16922fa4d1d2dbc2979365c857c11287ffc6da
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2025-05-22 10:50:51 -0300

    [REF] Acapra: Related Field + Button Editar

--- acapra/models/foto.py
+++ acapra/models/foto.py
@@ -3,7 +3,7 @@ from .animal import Animal
 
 
 class Foto(models.Model):
-    animal = models.ForeignKey(Animal, on_delete=models.CASCADE, related_name="animais")
+    animal = models.ForeignKey(Animal, on_delete=models.CASCADE, related_name="fotos")
     imagem = models.ImageField(upload_to="fotos_animais/", null=True, blank=True)
 
     def __str__(self):
