c4440cff1a78bf8116dd07849e243ed1e47d9d36
Rafael de Araujo da Silva <rafasilva3562@gmail.com>
2025-08-26 22:13:37 -0300

    [IMP] acapra: Adoções

---
+++ acapra/views/adotar_animal.py
@@ -0,0 +1,11 @@
+from django.shortcuts import redirect, get_object_or_404
+from acapra.models.animal import Animal
+
+
+def AdotarAnimal(request, animal_id):
+    animal = get_object_or_404(Animal, id=animal_id)
+    if request.method == "POST":
+        animal.status_adocao = "A"
+        animal.save()
+        return redirect("AdocoesAdmin")
+    return redirect("DashboardAdmin")
