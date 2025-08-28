e0e6c6382c59f81470c513d66eb12a75eaf71488
Rafael de Araujo da Silva <rafasilva3562@gmail.com>
2025-08-28 20:31:17 -0300

    [IMP]: acapra: Formulario Cadastro de animais

--- acapra/views/adocoes_admin.py
+++ acapra/views/adocoes_admin.py
@@ -3,9 +3,8 @@ from acapra.models.animal import Animal
 
 def AdocoesAdmin(request):
     query = request.GET.get("q", "")  # pega o termo buscado
-    animais = Animal.objects.filter(status_adocao="A")  # só adotados
-
+    animais = Animal.objects.filter(status_adocao="A") 
     if query:
-        animais = animais.filter(nome__icontains=query)  # filtra pelo nome
+        animais = animais.filter(nome__icontains=query)
 
     return render(request, "acapra/admin_adocoes.html", {"animais": animais, "query": query})
