f6765bfca70cdb28bf0560b69a87c6d449b8a52c
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2025-08-28 17:41:52 -0300

    rebase 22

--- acapra/views/adocoes_admin.py
+++ acapra/views/adocoes_admin.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from acapra.models.animal import Animal
 
+
 def AdocoesAdmin(request):
     animais = Animal.objects.filter(status_adocao="A")  # só adotados
     return render(request, "acapra/admin_adocoes.html", {"animais": animais})
